3c5236f516756f998aaafe6e97806bc783ff2394
Akilah Littlejohn <akilahlittlejohn@gmail.com>
2021-03-16 16:33:12 -0400

    refactor footer saas files, add mixins and variablbles to footer style, add svdg icon bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { WebDesignComponent } from './web-design/web-design.component';
+import { AppDesignComponent } from './app-design/app-design.component';
+import { GraphicDesignComponent } from './graphic-design/graphic-design.component';
+import { ContactComponent } from './contact/contact.component';
+import { LocationsComponent } from './locations/locations.component';
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'webdesign', component: WebDesignComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
